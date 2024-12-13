4b081e605f0d7c08fb35eb71452948120b7f103d
Evan Lee <feuxfollets1228@gmail.com>
2024-12-12 20:12:08 -0500

    C++

---
+++ cpp/stdc++.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <ranges>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
--- solution.cpp
+++
@@ -1,5 +0,0 @@
-import std;
-using namespace std;
-
-// Time: O()
-// Space: O()
