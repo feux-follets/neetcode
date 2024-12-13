bcbde140ebdcf575e3f1b1c349db408fcf9abc76
Evan Lee <feuxfollets1228@gmail.com>
2024-12-12 20:12:08 -0500

    C++

---
+++ cpp/stdc++.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <cstring>
+#include <deque>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <ranges>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
